d40905a4731f463974c9487c14c370f3995506ab
Feast <1353216996@qq.com>
2023-04-12 13:45:15 +0800

    Update preload.js

--- demo/src/preload.js
+++ demo/src/preload.js
@@ -1,4 +1,33 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("variableName", {});
+const BlueTooth = {
+    /**
+     * 选择id
+     * @param {string} id
+     * @returns
+     */
+    select(id) {
+        ipcRenderer.send("select-blueTooth", id);
+    },
+    /**
+     * 取消搜索
+     * @returns
+     */
+    cancel() {
+        ipcRenderer.send("cancel-blueTooth");
+    },
+    /**
+     * 收到来自ipcMain发送的设备列表
+     * @param {function<Array>} handler
+     * @returns
+     */
+    onBlueTooth(handler) {
+        ipcRenderer.on("blueTooth-list", handler);
+    },
+    offBlueTooth() {
+        ipcRenderer.off("blueTooth-list");
+    }
+}
+
+contextBridge.exposeInMainWorld("blueTooth", BlueTooth);
 window.onload = function () {};
