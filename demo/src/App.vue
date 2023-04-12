45b9fdfa7e4f5ca0d43da68111f3783d4e454198
Feast <1353216996@qq.com>
2023-04-12 13:45:11 +0800

    Update App.vue

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -1,18 +1,54 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <el-button @click="scan">搜索</el-button>
+    <el-scrollbar max-height="420">
+        <el-table :data="blueToothList" stripe>
+            <el-table-column prop="deviceId" label="Id" />
+            <el-table-column prop="deviceName" label="Name" />
+            <el-table-column label="Operate">
+                <template #default="scope">
+                    <el-button type="primary" @click="() => { select(scope.row) }">连接</el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+    </el-scrollbar>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import { BlueTooth } from './utils/blueToothUsing'
 export default {
     name: "App",
     components: {
-        HelloWorld,
     },
-    created(){
-      window.message = this.$message;
+    created() {
+        this.blueTooth.onBlueTooth(this.handler);
+    },
+    unmounted() {
+        this.blueTooth
+    },
+    data() {
+        return {
+            /**
+             * @type {BlueTooth}
+             */
+            blueTooth: window.blueTooth,//preload.js暴露的模块
+            blueToothList: []
+        }
+    },
+    methods: {
+        handler(sender, list) {
+            this.blueToothList = list;
+        },
+        scan() {
+            navigator
+            .bluetooth
+            .requestDevice({ acceptAllDevices: true })
+            .then(r=>{ console.log(r) })
+            .catch(e=>{ console.error(e) })
+        },
+        select(device) {
+            console.log(`You select ${device.deviceName}`)
+            this.blueTooth.select(device.deviceId);
+        }
     }
 };
 </script>
